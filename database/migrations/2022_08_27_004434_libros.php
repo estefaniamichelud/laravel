af5b7f072ae70d007b8052240f1f9389a18cf4af
estefaniamichelud <estefaniamichelud17@gmail.com>
2022-08-26 22:06:06 -0300

    mi primer commit

---
+++ database/migrations/2022_08_27_004434_libros.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Libros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("categoria_id");
+            $table->string("nombre");
+            $table->timestamps();
+            $table->foreign("categoria_id")->references("id")->on("categorias");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
